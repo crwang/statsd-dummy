059b3f806c757d227ff52ed42ae9773493411d10
Chris Wang <cwang@instructure.com>
2018-11-06 10:04:51 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Introduction
+
+This is a dummy udp app that simply echos the data sent to it.
+
+It is hard-coded in index.js to listen to 0.0.0.0:8125 for the statsd agent.
+
+# Running the server
+
+## Install the dependencies
+$ `npm install`
+
+## Run the server
+$ `npm start`
---
+++ index.js
@@ -0,0 +1,17 @@
+var PORT = 8125;
+var HOST = '0.0.0.0';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('Dummy Statsd Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+
+});
+
+server.bind(PORT, HOST);
